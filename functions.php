377d7558a40e3b96bfd033789510528e372cdd52
Tom Woodward <twwoodward@me.com>
2019-04-04 12:56:11 -0400

    Initial commit

---
+++ functions.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Understrap functions and definitions
+ *
+ * @package understrap
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+$understrap_includes = array(
+	'/theme-settings.php',                  // Initialize theme default settings.
+	'/setup.php',                           // Theme setup and custom theme supports.
+	'/widgets.php',                         // Register widget area.
+	'/enqueue.php',                         // Enqueue scripts and styles.
+	'/template-tags.php',                   // Custom template tags for this theme.
+	'/pagination.php',                      // Custom pagination for this theme.
+	'/hooks.php',                           // Custom hooks.
+	'/extras.php',                          // Custom functions that act independently of the theme templates.
+	'/customizer.php',                      // Customizer additions.
+	'/custom-comments.php',                 // Custom Comments file.
+	'/jetpack.php',                         // Load Jetpack compatibility file.
+	'/class-wp-bootstrap-navwalker.php',    // Load custom WordPress nav walker.
+	'/woocommerce.php',                     // Load WooCommerce functions.
+	'/editor.php',                          // Load Editor functions.
+	'/deprecated.php',                      // Load deprecated functions.
+);
+
+foreach ( $understrap_includes as $file ) {
+	$filepath = locate_template( 'inc' . $file );
+	if ( ! $filepath ) {
+		trigger_error( sprintf( 'Error locating /inc%s for inclusion', $file ), E_USER_ERROR );
+	}
+	require_once $filepath;
+}
+
+
+//custom stuff
+
+if(!function_exists('load_vcu_brandbar_script')){
+    function load_vcu_brandbar_script() {
+        global $post;
+        $version= '1.0'; 
+        $in_footer = false;
+        wp_enqueue_script('bsExpandJs', '//branding.vcu.edu/bar/academic/latest.js', null, $version, $in_footer);
+        wp_localize_script('my-script', 'my_script_vars', array(
+                'postID' => $post->ID
+            )
+        );
+    }
+}
+add_action('wp_enqueue_scripts', 'load_vcu_brandbar_script');
