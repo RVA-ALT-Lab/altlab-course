1ac641e0652fef263e5307671d7c6749fb29872e
Tom Woodward <twwoodward@me.com>
2019-04-16 14:24:45 -0400

    and maybe I'll get this right now

--- functions.php
+++ functions.php
@@ -190,9 +190,11 @@ function gform_stepper($entry, $form){
         $pre = 'zero-';
       }
       array_push($total_scores,$pre . $entry['gsurvey_score']);
-      array_push($total_guesses,$entry[3]);
+      array_push($total_guesses, $entry[3]);
+      var_dump($entry[3]);
+      var_dump($total_guesses);
+      print("<pre>".print_r($total_guesses,true)."</pre>");
     }    
-    print("<pre>".print_r($total_guesses,true)."</pre>");
       $gform_scores = array(          
            'scores' => $total_scores,
        );
